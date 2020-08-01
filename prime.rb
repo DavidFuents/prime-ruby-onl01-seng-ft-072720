503474b8c4ce06f551c6630ad218307cdc71572f
David Fuentes <davidf0827@gmail.com>
2020-08-01 22:40:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,3 @@
 def prime?(number)
-  test_nums = [2, 3, 5, 7, 11]
-  i = 0 
   
-  while i < test_nums.length
-    if (number %  test_nums[i]) == 0
-      true 
-    else 
-      false 
-    end
-  end
 end
