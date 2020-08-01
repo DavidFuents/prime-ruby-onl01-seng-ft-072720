9970a2ee120dba5137a5a75aa0d5b35f25a32422
David Fuentes <davidf0827@gmail.com>
2020-08-01 22:43:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if number < 2 
+    false 
+  elsif number == 2 
+    true 
+  elsif (2..number-1 ).each {|int| return false if number % int == 0}
+    true
+  end
+end
